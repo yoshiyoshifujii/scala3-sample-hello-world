febeba1c31fb3da8c80c9a13c9f2695e2e0b2469
Yoshitaka Fujii <yoshiyoshifujii@gmail.com>
2021-10-30 14:46:16 +0900

    add extension

--- src/main/scala/scala3_features/implicit/C.scala
+++ src/main/scala/scala3_features/implicit/C.scala
@@ -2,3 +2,9 @@ package scala3_features.`implicit`
 
 class C(value: String):
   val name: String = s"$value"
+
+extension [A](seq: Seq[A])
+  def tailOption: Option[Seq[A]] =
+    if seq.nonEmpty then Some(seq.tail) else None
+  def secondOption: Option[A] =
+    tailOption.flatMap(_.headOption)
--- src/test/scala/scala3_features/implicit/CSpec.scala
+++ src/test/scala/scala3_features/implicit/CSpec.scala
@@ -8,3 +8,8 @@ class CSpec:
     def m(v: Int)(using c: C): String = s"$v, ${c.name}"
     given c: C = C("hoge")
     assertEquals("100, hoge", m(100))
+
+  @Test def t2(): Unit =
+    assertEquals(None, Seq.empty.secondOption)
+    assertEquals(Some(2), Seq(1,2,3).secondOption)
+
